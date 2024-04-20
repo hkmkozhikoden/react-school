1355f04d236d5ab71ece72a0c74c37f385d1abbf
hakeem <hakeem@codeap.com>
2024-04-20 17:23:36 +0530

    gallery issue fixing

--- src/compontent/galleryCard.jsx
+++ src/compontent/galleryCard.jsx
@@ -9,27 +9,25 @@ import Gallery5 from "../assets/img/img/img13.jpg";
 import Gallery6 from "../assets/img/home/banner1.jpg";
 import Gallery7 from "../assets/img/home/banner2.jpg";
 import Gallery8 from "../assets/img/home/2.jpeg";
-import '../style/gallery.css'
-
-
-
+import "../style/gallery.css";
 
 function Gallery(props) {
   const containerRef = useRef(null);
 
   useEffect(() => {
     const container = containerRef.current;
-
+  
     const delegate = props.delegate || "[data-fancybox]";
     const options = props.options || {};
-
+  
     NativeFancybox.bind(container, delegate, options);
-
+  
     return () => {
       NativeFancybox.unbind(container);
       NativeFancybox.close();
     };
-  }, []);
+  }, [props.delegate, props.options]); // Include props.delegate and props.options in the dependency array
+  
 
   return <div ref={containerRef}>{props.children}</div>;
 }
@@ -42,45 +40,90 @@ function App() {
           <div className="col-md-4 mb-2 mt-2 mb-md-3 mt-md-3">
             <div className="gallery-card">
               <Gallery className="W-100">
-                  <a data-fancybox="gallery1" className="gallery-image" href={Gallery1}>
-                   <img src={Gallery1} className="gallery-image" alt="galley 3" />
-                 </a>
-                 <a data-fancybox="gallery1" href={Gallery2} className="gallery-image"></a>
-                 <a data-fancybox="gallery1" href={Gallery3} className="gallery-image"></a>
-                 <a data-fancybox="gallery1" href={Gallery4} className="gallery-image"></a>
-               </Gallery>
+                <a
+                  data-fancybox="gallery1"
+                  className="gallery-image"
+                  href={Gallery1}
+                >
+                  <img
+                    src={Gallery1}
+                    className="gallery-image"
+                    alt="galley 3"
+                  />
+                  <span className="visually-hidden">View Image 1</span>
+                </a>
+                <a data-fancybox="gallery1" href={Gallery2} className="gallery-image">
+                  <span className="visually-hidden">View Image 2</span>
+                </a>
+                <a data-fancybox="gallery1" href={Gallery3} className="gallery-image">
+                  <span className="visually-hidden">View Image 3</span>
+                </a>
+                <a data-fancybox="gallery1" href={Gallery4} className="gallery-image">
+                  <span className="visually-hidden">View Image 4</span>
+                </a>
+              </Gallery>
               <div className="card-content">
-              <h6 className="mt-3">JDT EMPOWERED WEAVES INAUGURAL SESSION</h6>
+                <h6 className="mt-3">JDT EMPOWERED WEAVES INAUGURAL SESSION</h6>
               </div>
             </div>
           </div>
           <div className="col-md-4 mb-2 mt-2 mb-md-3 mt-md-3">
             <div className="gallery-card">
-              <Gallery>
-               <a data-fancybox="gallery2" href={Gallery8}>
-                 <img src={Gallery8} className="gallery-image" alt="gallety 2" />
-               </a>
-               <a data-fancybox="gallery2" className="gallery-image" href={Gallery5}></a>
-               <a data-fancybox="gallery2" className="gallery-image" href={Gallery5}></a>
-               <a data-fancybox="gallery2" className="gallery-image" href={Gallery5}></a>
+              <Gallery className="W-100">
+                <a
+                  data-fancybox="gallery1"
+                  className="gallery-image"
+                  href={Gallery2}
+                >
+                  <img
+                    src={Gallery2}
+                    className="gallery-image"
+                    alt="galley 3"
+                  />
+                  <span className="visually-hidden">View Image 1</span>
+                </a>
+                <a data-fancybox="gallery1" href={Gallery3} className="gallery-image">
+                  <span className="visually-hidden">View Image 2</span>
+                </a>
+                <a data-fancybox="gallery1" href={Gallery4} className="gallery-image">
+                  <span className="visually-hidden">View Image 3</span>
+                </a>
+                <a data-fancybox="gallery1" href={Gallery5} className="gallery-image">
+                  <span className="visually-hidden">View Image 4</span>
+                </a>
               </Gallery>
               <div className="card-content">
-               <h6 className="mt-3">JDT EMPOWERED WEAVES INAUGURAL SESSION</h6>
+                <h6 className="mt-3">JDT EMPOWERED WEAVES INAUGURAL SESSION</h6>
               </div>
             </div>
           </div>
           <div className="col-md-4 mb-2 mt-2 mb-md-3 mt-md-3">
             <div className="gallery-card">
-              <Gallery>
-               <a data-fancybox="gallery2" className="gallery-image" href={Gallery7}>
-                 <img src={Gallery7} className="gallery-image" alt="galley 1" />
-               </a>
-               <a data-fancybox="gallery2" className="gallery-image" href={Gallery6}></a>
-               <a data-fancybox="gallery2" className="gallery-image" href={Gallery6}></a>
-               <a data-fancybox="gallery2" className="gallery-image" href={Gallery6}></a>
+              <Gallery className="W-100">
+                <a
+                  data-fancybox="gallery1"
+                  className="gallery-image"
+                  href={Gallery8}
+                >
+                  <img
+                    src={Gallery8}
+                    className="gallery-image"
+                    alt="galley 3"
+                  />
+                  <span className="visually-hidden">View Image 1</span>
+                </a>
+                <a data-fancybox="gallery1" href={Gallery4} className="gallery-image">
+                  <span className="visually-hidden">View Image 2</span>
+                </a>
+                <a data-fancybox="gallery1" href={Gallery5} className="gallery-image">
+                  <span className="visually-hidden">View Image 3</span>
+                </a>
+                <a data-fancybox="gallery1" href={Gallery6} className="gallery-image">
+                  <span className="visually-hidden">View Image 4</span>
+                </a>
               </Gallery>
               <div className="card-content">
-               <h6 className="mt-3">JDT EMPOWERED WEAVES INAUGURAL SESSION</h6>
+                <h6 className="mt-3">JDT EMPOWERED WEAVES INAUGURAL SESSION</h6>
               </div>
             </div>
           </div>
